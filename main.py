2a7550c78740ebffc1f4b4f95a45f733a6f16c62
Aleksey Gladkov <ja007-ltag@yandex.ru>
2022-05-25 23:01:23 +0300

    Add classes Exception

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+# --- Классы исключений ---
+class BoardException(Exception):
+    pass
+
+
+class BoardOutException(BoardException):
+    def __str__(self):
+        return "Вы пытаетесь выстрелить за доску!"
+
+
+class BoardUserException(BoardException):
+    def __str__(self):
+        return "Вы уже стреляли в эту клетку"
+
+
+class BoardWrongShipException(BoardException):
+    pass
+
+
 class Dot:
     def __init__(self, x, y):
         self.x = x
